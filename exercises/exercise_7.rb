24c85529e963e6c48c290d14ce3fad394729a6fe
Angela Zhou <a03zhou@gmail.com>
2023-12-05 19:31:51 -0500

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter a store name."
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+if new_store.save
+  puts "Store created successfully."
+else
+  puts "The following error(s) occurred:"
+  new_store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end
